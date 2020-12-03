79fc98d50f24d9fc6666de9c6e9dfcb3f1cdfdf7
shihaka <att253@ya.ru>
2020-12-04 03:16:43 +0400

    Animals

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Animal dog = new Animal();
+        dog.name = "Alf";
+        dog.sound = "woff";
+        dog.age = 3;
+        dog.onDisplay();
+
+        Animal cat = new Animal();
+        cat.name = "Murka";
+        cat.sound = "meow";
+        cat.weight = 4;
+        cat.onDisplay();
+    }
+
+    static class Animal
+    {
+        String name;
+        String sound;
+        int weight;
+        int age;
+
+        void onDisplay(){
+        System.out.printf("Name: %s \tSound: %s \tWeight: %d \tAge: %d\n", name, sound, weight, age);
+        }
     }
 }
